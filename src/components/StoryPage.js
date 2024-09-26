ac38092f9e6325ffc5c97abff71a3eec13867ea4
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-26 18:17:16 +0530

    Update StoryPage.js

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -23,7 +23,7 @@ const StoryPage = () => {
         onClick={() => navigate(-1)}
         className={`absolute top-2 left-2 md:top-4 md:left-4 px-2 md:px-4 py-1 md:py-2 rounded text-white focus:outline-none`}
       >
-        <img src={back} alt="Back" className="w-12 h-12 md:w-16 md:h-16 object-cover rounded" />
+        <img src={back} loading="lazy" alt="Back" className="w-12 h-12 md:w-16 md:h-16 object-cover rounded" />
       </button>
       <button
         onClick={handleToggleTheme}
@@ -33,7 +33,7 @@ const StoryPage = () => {
       </button>
       <div className={`w-full max-w-4xl p-4 md:p-6 rounded-lg shadow-lg ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
         <div className='flex flex-col md:flex-col items-center  gap-4 md:gap-8'>
-          <img src={sideImage} alt="Side" className="w-32 h-32 md:w-40 md:h-40 object-cover rounded" />
+          <img src={sideImage} loading="lazy" alt="Side" className="w-32 h-32 md:w-40 md:h-40 object-cover rounded" />
           <div className="flex flex-col items-center ">
             <h1 className="text-2xl md:text-4xl font-semibold mb-4 text-center md:text-left mt-4 md:mt-8 font-poppins">Your Generated Story</h1>
             <div className="flex items-start ml-4 mr-4 md:ml-6 md:mr-6 gap-4">
