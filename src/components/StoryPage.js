660fc0fac3970cc6d9a7274d1479b82487176347
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-11-24 06:15:34 +0530

    Update StoryPage.js

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -32,10 +32,10 @@ const StoryPage = () => {
       >
         {darkMode ? 'Light Mode' : 'Dark Mode'}
       </button>
-      <div className={`w-full max-w-4xl p-4 md:p-6 rounded-lg shadow-lg ${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+      <div className="w-full max-w-4xl p-4 md:p-6 rounded-lg shadow-lg">
         <div className='flex flex-col md:flex-col items-center gap-4 md:gap-8'>
           <img src={sideImage} loading="lazy" alt="Side" className={`w-32 h-32 md:w-40 md:h-40 object-cover rounded ${darkMode ? 'bg-yellow-400' : 'bg-gray-700'} `} />
-            <Share story={story} /> 
+            <Share story={story} theme={darkMode} /> 
           </div>
           <div className="flex flex-col items-center">
             <h1 className="text-2xl md:text-4xl font-semibold mb-4 text-center md:text-left mt-4 md:mt-8 font-poppins">Your Generated Story</h1>
