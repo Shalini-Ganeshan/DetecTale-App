252f8ef2538a4224d87132b585da2a861a874e69
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-07-26 13:18:02 +0530

    Update WriteCharacter.js

--- src/components/WriteCharacter.js
+++ src/components/WriteCharacter.js
@@ -8,7 +8,7 @@ const MAX_CHARACTERS = 12;
 
 const languageOptions = [
   { value: 'en', label: 'English' },
-  { value: 'ta', label: 'Tamil' },
+ { value: 'tamil', label: 'Tamil' },
   { value: 'te', label: 'Telugu' },
   { value: 'kn', label: 'Kannada' },
   { value: 'ml', label: 'Malayalam' },
